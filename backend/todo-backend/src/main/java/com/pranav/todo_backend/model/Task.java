b79f053b98a6054d2da606ddf075d374c194505d
pranavdeep246-stack <pranavdeep246@gmail.com>
2026-06-01 13:45:37 +0530

    Task.java done

---
+++ backend/todo-backend/src/main/java/com/pranav/todo_backend/model/Task.java
@@ -0,0 +1,48 @@
+package com.pranav.todo_backend.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tasks")
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private boolean completed;
+
+    public Task() {
+    }
+
+    public Task(String title, boolean completed) {
+        this.title = title;
+        this.completed = completed;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+}
