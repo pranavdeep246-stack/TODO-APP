38adc81ca36a23ee2ec6fb4370a0abb6a299387a
pranavdeep246-stack <pranavdeep246@gmail.com>
2026-06-01 21:25:47 +0530

    adding loadTask

--- script.js
+++ script.js
@@ -2,6 +2,84 @@ let input=document.querySelector("#Taskinput");
 let add=document.querySelector("#add");
 let main=document.querySelector(".main");
 
+async function loadTasks() {
+
+    const response = await fetch("http://localhost:8080/tasks");
+
+    const tasks = await response.json();
+
+    tasks.forEach(taskObj => {
+
+        let newdiv = document.createElement("div");
+        newdiv.style.display = "flex";
+
+        let tick = document.createElement("input");
+        tick.type = "checkbox";
+        tick.checked = taskObj.completed;
+
+        let task = document.createElement("textarea");
+        task.value = taskObj.title;
+        task.readOnly = true;
+
+        let cross = document.createElement("button");
+        cross.innerText = "✖";
+
+        tick.classList.add("tick");
+        newdiv.classList.add("newdiv");
+        task.classList.add("task");
+        cross.classList.add("cross");
+
+        newdiv.append(tick);
+        newdiv.append(task);
+        newdiv.append(cross);
+
+        main.append(newdiv);
+
+        tick.addEventListener("change",()=>{
+    if(tick.checked){
+    task.style.transform="scale(0.9)";
+     task.style.opacity="0.8";
+     task.style.textDecoration="line-through";
+}
+else{
+    task.style.transform="scale(1)";
+     task.style.opacity="1";
+     task.style.textDecoration="none";
+}});
+
+ cross.addEventListener("click", () => {
+    newdiv.remove();
+});
+task.addEventListener("click",()=>{
+    if(task.readOnly==true){
+        task.blur();
+    }
+});
+
+task.addEventListener("dblclick",()=>{
+    task.readOnly=false;
+    task.style.cursor="text";
+    task.focus();
+});
+
+task.addEventListener("blur",()=>{
+    task.readOnly=true;
+    task.style.cursor="default";
+});
+task.addEventListener("input", () => {
+    task.style.height = "auto";
+    task.style.height = task.scrollHeight + "px";
+});
+task.addEventListener("blur",()=>{
+    if(task.value.trim()==""){
+        newdiv.remove();
+    }
+});
+
+    });
+}
+
+
 async function taskaddition(){
  if(input.value.trim()==""){alert("Enter some text"); return;}
 
@@ -99,4 +177,4 @@ input.addEventListener("keydown",(event)=>{
     if(event.key==="Enter"){
    taskaddition(); }
 });
-
+ loadTasks();
