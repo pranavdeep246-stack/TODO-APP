190b905599f71208a0bc0176c993ca2e986b0ed5
pranavdeep246-stack <pranavdeep246@gmail.com>
2026-06-01 14:14:11 +0530

    adding getmapping

---
+++ backend/todo-backend/src/main/java/com/pranav/todo_backend/controller/TaskController.java
@@ -0,0 +1,24 @@
+package com.pranav.todo_backend.controller;
+
+import com.pranav.todo_backend.model.Task;
+import com.pranav.todo_backend.repository.TaskRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/tasks")
+@CrossOrigin("*")
+public class TaskController {
+
+    private final TaskRepository taskRepository;
+
+    public TaskController(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    @GetMapping
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+}
